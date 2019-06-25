53dd50b98c24fb4963307017a226c72fe1f29603
Prashant Singh <iprashant2402@gmail.com>
2019-06-26 03:42:17 +0530

    Final Commit

---
+++ precache-manifest.6d2883e7f9b6e0d3ddc6792664a42e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb4185a0e3501bc3a4bfda8656bdbed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d49ff336af707189c82",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "abc4a73725f7fbbf4a68",
+    "url": "/static/css/main.377420f3.chunk.css"
+  },
+  {
+    "revision": "1d49ff336af707189c82",
+    "url": "/static/js/2.a62565a7.chunk.js"
+  },
+  {
+    "revision": "abc4a73725f7fbbf4a68",
+    "url": "/static/js/main.bb2b9093.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
